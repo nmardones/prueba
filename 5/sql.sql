94351cf8a03544a3b5bf1213d693412d6396fb3a
nmardones <nmardones.fabre.21@gmail.com>
2015-07-22 15:03:01 +0000

    sql

---
+++ 5/sql.sql
@@ -0,0 +1,11 @@
+
+
+select * from users;
+
+delete from users where id = 37;
+
+insert into users (nombres ,apellidos) 
+	values ('nesotr','mardones');
+    
+update users set nombres = 'Néor'
+		where id = 38;
